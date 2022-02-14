49b0511e9c15e5bca05035411838d06e466071f6
Marcin Iwaniak <miwaniak@betacom.com.pl>
2022-02-14 16:37:43 +0100

    Initial neogit config

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -107,7 +107,7 @@ local mappings = {
 
   g = {
     name = "Git",
-    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+    g = { "<cmd>lua require 'neogit'.open()<cr>", "Neogit" },
     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
